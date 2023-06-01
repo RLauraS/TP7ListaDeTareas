ab8741cc29a3f9606f33b8b883398eae6f35b38b
RLauraS <R_LauraS@yahoo.com.ar>
2023-06-01 07:29:21 -0300

    Se inica con la carga de tareas en el formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,17 +1,35 @@
-import { Form,Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 const FormularioTarea = () => {
-    return (
-        <>
-        <Form>
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //tareas.push (Es un metodo mutable, y NO SE PUEDE MODIFICAR UN STATE, en cambio si fuera un array normal guardado en cualquier variable si podría hacerlo)
+    setTareas([...tareas, tarea])
+    setTarea('')
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
-          <Button variant="primary">Agregar</Button>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e)=>setTarea(e.target.value)}
+            value={tarea}/>
+            
+          <Button variant="primary" type="submit">
+            Agregar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTareas></ListaTareas>
-        </>
-    );
+    </>
+  );
 };
 
 export default FormularioTarea;
