f0b266078fd9a5f07370968715e1c0cb49465030
RLauraS <R_LauraS@yahoo.com.ar>
2023-06-01 06:34:36 -0300

    Agregamos el maquetado de los botones

---
+++ src/components/FormularioTarea.jsx
@@ -0,0 +1,17 @@
+import { Form,Button } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
+const FormularioTarea = () => {
+    return (
+        <>
+        <Form>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Button variant="primary">Agregar</Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
+        </>
+    );
+};
+
+export default FormularioTarea;
---
+++ src/components/ItemTarea.jsx
@@ -0,0 +1,10 @@
+import { ListGroup,Button } from "react-bootstrap";
+const ItemTarea = () => {
+    return (
+        <ListGroup.Item className="d-flex justify-content-between">Ejemplo de tarea
+            <Button variant="danger">Borrar</Button>
+        </ListGroup.Item>
+    );
+};
+
+export default ItemTarea;
